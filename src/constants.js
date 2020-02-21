f2220794511f31081b886991ec2eb77259012386
Wallace Batista <wallacebatistaoliveira@gmail.com>
2020-02-21 20:34:50 -0300

    app: remove unnecessary env vars

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,4 @@ export const FIREBASE_SETTINGS = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 }
